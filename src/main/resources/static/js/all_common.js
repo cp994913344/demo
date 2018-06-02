7ed93cc4a7eb9185195e571b97031774a2bf131f
cpGit <994913344@qq.com>
2018-06-02 14:22:37 +0800

    登录页面

--- src/main/resources/static/js/all_common.js
+++ src/main/resources/static/js/all_common.js
@@ -1,23 +1,18 @@
-// function htmlOnload() {
-//     debugger;
-//     $.ajax({
-//         url: '/returnLogin',
-//         type: 'post',
-//         success: function (_res) {
-//             debugger;
-//             if (_res.message) {
-//                 $("#login_user_name").text(_res.message);
-//                 $("#pl-login-info").removeClass("displayNone");
-//             } else {
-//                 $("#loginDiv").removeClass("displayNone");
-//             }
-//         }
-//     });
-// }
-//
-// //控制头像登录 滑入滑出
-// $("#pl-login-info").hover(function(){
-//     $("#loginInfo").toggle();
-// });
-
-alert("12321");
+window.onload=function(){
+    $.ajax({
+        url: '/returnLogin',
+        type: 'post',
+        success: function (_res) {
+            if (_res.code===200) {
+                $("#login_user_name").text(_res.message);
+                $("#pl-login-info").removeClass("displayNone");
+            } else if (_res.code===500) {
+                $("#loginDiv").removeClass("displayNone");
+            }
+        }
+    });
+}
+//控制头像登录 滑入滑出
+$("#pl-login-info").hover(function(){
+    $("#loginInfo").toggle();
+});
