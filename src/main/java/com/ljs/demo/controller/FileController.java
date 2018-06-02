92d61a8b617b673ed49f99ebb7158dc9d5aa69c4
liujunsheng <liu_js@suixingpay.com>
2018-06-02 16:42:18 +0800

    修改保存文件路径，文件上传接口

--- src/main/java/com/ljs/demo/controller/FileController.java
+++ src/main/java/com/ljs/demo/controller/FileController.java
@@ -3,7 +3,9 @@ package com.ljs.demo.controller;
 
 import com.ljs.demo.Service.FileService;
 import com.ljs.demo.common.constant.GetUuid;
+import com.ljs.demo.common.constant.redis.RedisClient;
 import com.ljs.demo.common.response.ResponseMessage;
+import com.ljs.demo.common.utils.StaticClass;
 import com.ljs.demo.pojo.domain.MFile;
 import com.ljs.demo.pojo.domain.Visitor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 import java.io.File;
 import java.io.IOException;
 import java.util.Date;
@@ -25,6 +28,9 @@ public class FileController {
 
     @Autowired
     FileService fileService;
+
+    @Autowired
+    RedisClient redisClient;
 
     /**
      * 根据ID查询
@@ -57,22 +63,44 @@ public class FileController {
     }
 
     /**
-     * Visitor游客头像上传
-     *
-     * @param file request
+     * 生成文件路径，并保存到images文件夹
+     * @param file
+     * @param request
      * @return
+     * @throws IOException
      */
-    @RequestMapping(value = "/uploadPhoto")
-    public ResponseMessage uploadPhoto(@RequestParam("file") MultipartFile file , Visitor visitor , HttpServletRequest request) throws IOException {
-        log.info("|对外接口|入参[{}]", visitor);
+    @RequestMapping("/savaPhoto")
+    public ResponseMessage savePhoto(@RequestParam("file") MultipartFile file, HttpServletRequest request) throws IOException {
+        log.info("|对外接口|入参[{}]", file);
         if(file.isEmpty()){
             return ResponseMessage.error("文件不能为空");
         }
         String path = request.getServletContext().getRealPath("/images/");
         String name = file.getOriginalFilename();
-
         String filename = path + GetUuid.uuid+name;
         log.info("|文件存储路径|[{}]",filename);
+        file.transferTo(new File(filename));
+        return ResponseMessage.ok("文件存储路径",filename);
+    }
+
+    /**
+     * Visitor游客头像上传
+     *
+     * @param filename
+     * @return
+     */
+    @RequestMapping(value = "/uploadPhoto")
+    public ResponseMessage uploadPhoto(@RequestParam("filename")  String filename, HttpServletRequest request) throws Exception {
+        log.info("|对外接口|入参[{}]", filename);
+        if(filename.isEmpty()){
+            return ResponseMessage.error("文件路径不能为空");
+        }
+        HttpSession session = request.getSession();
+        String email = (String) session.getAttribute(StaticClass.LOGIN_CODE);
+        if(email == null){
+            return ResponseMessage.error("用户未登录");
+        }
+        Visitor visitor = (Visitor) redisClient.get(email+ StaticClass.LOGIN_CODE);
         int i = 0;
         if (visitor.getVisitorid() != null){
             MFile f = new MFile();
@@ -84,7 +112,6 @@ public class FileController {
 
             i = fileService.insert(f);
         }
-        file.transferTo(new File(filename));
         if(i == 0){
             return ResponseMessage.error("存储失败");
         }
