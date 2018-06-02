7ebf56d675ec0235cc4512aa220d4ea6bed6c4c4
cpGit <994913344@qq.com>
2018-06-02 20:42:10 +0800

    登录页面

--- src/main/resources/static/js/all_common.js
+++ src/main/resources/static/js/all_common.js
@@ -6,6 +6,7 @@ window.onload=function(){
             if (_res.code===200) {
                 $("#login_user_name").text(_res.message);
                 $("#pl-login-info").removeClass("displayNone");
+                userName=_res.message;
             } else if (_res.code===500) {
                 $("#loginDiv").removeClass("displayNone");
             }
